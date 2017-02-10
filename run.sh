9a816f14a6b9ff27d897c2f2b7ee92512c2a367e
Cullen Taylor <CullenTaylor@outlook.com>
2017-02-10 15:22:24 -0600

    Fixes to replay_processing
    * make sure python3 is being installed and used.
    * change name of util to sc2_util.
    * ensure replays docker volume exists and necessary docker images are
      pulled.
    * Combines sc2_base and replay_processing docker images.
    
    Signed-off-by: Cullen Taylor <CullenTaylor@outlook.com>

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+docker pull eggshell/replay_processing
+docker pull eggshell/sc2_util
+docker volume create replays
 docker run -dit --name="sc2_util" -v /var/run/docker.sock:/var/run/docker.sock -v replays:/replays util
